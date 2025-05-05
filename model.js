212c7cd069152d7bc115915f9c667b27168c28fb
MitaliM13 <mitalimaurya@kccemsr.edu.in>
2025-05-05 16:25:31 +0530

    Updated model

--- model.js
+++ model.js
@@ -19,6 +19,10 @@ const UserSchema = new Mongoose.Schema({
     tasksCreated: {
         type: [],
         default: []
+    },
+    taskAssigned: {
+        type: [],
+        default: []
     }
 })
 
