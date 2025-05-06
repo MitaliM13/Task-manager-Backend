797dffbbb7cf372df2182024bd53d76fa8cb3a22
MitaliM13 <mitalimaurya@kccemsr.edu.in>
2025-05-06 20:41:53 +0530

    display assigned and created tasks

--- Routes/user.js
+++ Routes/user.js
@@ -1,15 +1,21 @@
-import express from 'express'
-import { Users } from '../model.js'
-const router = express.Router()
+import express from "express";
+import { Users } from "../model.js";
+const router = express.Router();
 
-router.get('/', async (req, res) => {
-    try {
-        const users = await Users.find({}, 'username email')
-        res.json(users)
-    } catch (error) {
-        res.status(500).json({ message: "Failed to fetch users", error })
-    }
-})
+router.get("/", async (req, res) => {
+  try {
+    const users = await Users.find(
+      {},
+      "username email tasksCreated taskAssigned"
+    )
+      .populate("taskAssigned", "title description")
+      .populate("tasksCreated", "title description");
 
+    res.json(users);
+  } catch (error) {
+    console.error("Error fetching users:", error);
+    res.status(500).json({ message: "Failed to fetch users", error });
+  }
+});
 
-export default router
+export default router;
